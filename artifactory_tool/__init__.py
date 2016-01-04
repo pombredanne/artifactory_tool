8abae5a098b1ba818379214d9c515a87bef66e88
Sean Abbott <sean.abbott@datarobot.com>
2016-01-04 14:23:32 -0500

    Bump version: 0.1.0 → 0.2.0

--- artifactory_tool/__init__.py
+++ artifactory_tool/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = 'Sean Abbott'
 __email__ = 'sean.abbott@datarobot.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .api import get_artifactory_config_from_url, update_ldapSettings_from_dict, update_artifactory_config, cr_repository, update_password
